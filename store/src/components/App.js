05fe6c298cb0970dd9cc39726221a022d230371a
Eliana Henderson <inako215@gmail.com>
2023-01-15 08:28:36 -0800

    Added bootstrap and sass, got basic navbar rendering

---
+++ store/src/components/App.js
@@ -0,0 +1,13 @@
+import NavBar from "./nav-bar/NavBar";
+
+const App = () => {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <NavBar />
+      </header>
+    </div>
+  );
+};
+
+export default App;
